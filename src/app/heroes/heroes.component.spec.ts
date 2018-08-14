f078b251b9b63e2bab1c6ebc37b7e347b090c52e
Mateusz Kłosiński <mkklosin@gmail.com>
2018-08-14 08:40:12 +0200

    Hero component on select changes, mocked heroes added

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -4,13 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes.component';
 
 describe('HeroesComponent', () => {
+  const heroes = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+  ];
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ FormsModule ],
-      declarations: [ HeroesComponent ]
+      declarations: [ HeroesComponent ],
+      providers: [
+        { provide: 'heroes', useValue: heroes }
+      ]
     })
     .compileComponents();
   }));
@@ -27,6 +34,12 @@ describe('HeroesComponent', () => {
 
   it('should render hero list', () => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div').textContent).toContain('1 WINDSTORM');
+    expect(compiled.querySelector('.item > .item__badge').textContent).toContain('11');
+    expect(compiled.querySelector('.item > .item__hero').textContent).toContain('Mr. Nice');
+  });
+
+  it('should render proper h2 title', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('My Heroes');
   });
 });
