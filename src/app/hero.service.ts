94b3c98b883de4c26fc581a061106b84e7c8b95a
Mateusz Kłosiński <mkklosin@gmail.com>
2018-08-16 11:14:24 +0200

    Add new hero, hero hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,6 +42,25 @@ export class HeroService {
       );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+   return this.httpClient.post<Hero>(this.heroesUrl, hero, httpOptions)
+     .pipe(
+       tap((hero: Hero) => this.log(`added hero /w id = ${hero.id}`)),
+       catchError(this.handleError<Hero>('addHero'))
+     );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.httpClient.delete<Hero>(url, httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id = ${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
