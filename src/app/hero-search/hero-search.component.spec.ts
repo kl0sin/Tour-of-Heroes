da21c293a11560499bf31d243a33759323e8fb09
Mateusz Kłosiński <mkklosin@gmail.com>
2018-08-21 10:33:33 +0200

    Failing tests fix

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HeroSearchComponent } from './hero-search.component';
 
@@ -8,7 +10,13 @@ describe('HeroSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ] ,
+      declarations: [
+        HeroSearchComponent
+      ]
     })
     .compileComponents();
   }));
